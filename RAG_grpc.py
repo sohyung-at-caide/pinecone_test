693c3329bee1ac1b3129c9d7c75cb22f2fe385c3
Sohyo <cienakim12@gmail.com>
2024-11-28 14:57:50 +0100

    clean up

--- RAG_grpc.py
+++ RAG_grpc.py
@@ -1,15 +1,15 @@
-import yaml
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from pinecone.grpc import PineconeGRPC
 from langchain_pinecone import PineconeVectorStore, PineconeEmbeddings
-import time
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain import hub
-import os
 from langchain_anthropic import ChatAnthropic
+import os
+import yaml
+import time
 
 
 def get_config(config: str):
@@ -17,7 +17,6 @@ def get_config(config: str):
     with open(f"{config}.yaml", "r") as yaml_file:
         config = yaml.safe_load(yaml_file)
     return config
-
 
 
 config = get_config("config")
@@ -53,16 +52,12 @@ if index_name not in pc.list_indexes().names():
         metric="cosine",
         spec={'serverless': {'cloud': 'aws', 'region': 'us-east-1'}}
     )
-    # Wait for index to be ready
     while not pc.describe_index(index_name).status['ready']:
         time.sleep(1)
-print("")
-# See that it is empty
-# print("Index before upsert:")
-# print(pc.Index(index_name).describe_index_stats())
-# print("\n")
 
-namespace = "wondervector5000"
+
+namespace = "internal_guides"
+# namespace = "wondervector5000"
 
 docsearch = PineconeVectorStore.from_documents(
     documents=chunks,
@@ -71,16 +66,7 @@ docsearch = PineconeVectorStore.from_documents(
     namespace=namespace
 )
 
-time.sleep(5)
-
-# See how many vectors have been upserted
-# print("Index after upsert:")
-# print(pc.Index(index_name).describe_index_stats())
-# print("\n")
-# time.sleep(2)
-
 index = pc.Index(index_name)
-
 for ids in index.list(namespace=namespace):
     query = index.query(
         id=ids[0],
@@ -92,35 +78,35 @@ for ids in index.list(namespace=namespace):
     print(query)
     print("\n")
 
-
 retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 retriever = docsearch.as_retriever()
 
-llm = ChatOpenAI(
+openai_llm = ChatOpenAI(
     openai_api_key=config['open-ai-key'],
-    model_name='gpt-4o',
+    model_name='gpt-4o-mini',
     temperature=0.0
 )
 
-chat = ChatAnthropic(
-    temperature=0,
-    api_key=config['sonnet-key'],
+claude_llm = ChatAnthropic(
+    temperature=0.0,
+    api_key=os.environ.get('ANTHROPIC_API_KEY'),
     model_name="claude-3-opus-20240229"
 )
 
 combine_docs_chain = create_stuff_documents_chain(
-    llm, retrieval_qa_chat_prompt
+    openai_llm, retrieval_qa_chat_prompt
 )
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
 query1 = "How can I update the tone of voice?"
-query2 = "How can I update the tone of voice?"
-
-answer1_without_knowledge = llm.invoke(query1)
-
+answer1_without_knowledge = openai_llm.invoke(query1)
 print("Query 1:", query1)
 print("\nAnswer without knowledge:\n\n", answer1_without_knowledge.content)
 print("\n")
+
+# openai_llm.invoke("start the LLM")
+query2 = "Where I can see all my email thread?"
+
 start_time = time.time()
 answer2_with_knowledge = retrieval_chain.invoke({"input": query2})
 duration = time.time() - start_time
@@ -128,6 +114,6 @@ duration = time.time() - start_time
 print("\nAnswer with knowledge:\n\n", answer2_with_knowledge['answer'])
 print("\nContext Used:\n\n", answer2_with_knowledge['context'])
 print("\n")
-time.sleep(2)
 print(f'time : {duration}')
+
 pc.delete_index(index_name)
